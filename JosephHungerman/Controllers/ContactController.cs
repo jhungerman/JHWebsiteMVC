99e5d04eedc6cd7c049ac03516015f0549fa1b59
jhungerman <joseph_hungerman@yahoo.com>
2022-04-08 02:23:00 -0500

    Completed Contact controller and page functionality

--- JosephHungerman/Controllers/ContactController.cs
+++ JosephHungerman/Controllers/ContactController.cs
@@ -1,10 +1,49 @@
-﻿namespace JosephHungerman.Controllers
+﻿using JosephHungerman.Data.Repositories;
+using JosephHungerman.Models;
+using JosephHungerman.Models.Dtos;
+using JosephHungerman.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JosephHungerman.Controllers
 {
-    public class ContactController : BaseApiController
+    public class ContactController : Controller
     {
-        public ContactController()
+        private readonly IContactService _service;
+
+        public ContactController(IContactService service)
         {
-            
+            _service = service;
+        }
+
+        [HttpPost("SendMessage")]
+        public async Task<IActionResult> SendMessage(MessageDto message)
+        {
+            try
+            {
+                var response = await _service.AddMessageAsync(message);
+
+                if (response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(MessageSuccess));
+                }
+
+                return RedirectToAction("Error");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public IActionResult MessageSuccess()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel());
         }
     }
 }
--- JosephHungerman/Extensions/ServiceCollectionExtensions.cs
+++ JosephHungerman/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using JosephHungerman.Data;
 using JosephHungerman.Data.Repositories;
+using JosephHungerman.Helpers;
+using JosephHungerman.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace JosephHungerman.Extensions
@@ -10,6 +12,7 @@ namespace JosephHungerman.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<ApplicationDbContext>(options =>
             {
                 options.UseMySql(
@@ -17,6 +20,7 @@ namespace JosephHungerman.Extensions
                     new MariaDbServerVersion(new Version(10, 3)));
             });
             services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IContactService, ContactService>();
             return services;
         }
     }
