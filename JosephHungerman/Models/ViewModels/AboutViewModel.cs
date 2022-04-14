7fa922e0ba78d6f9510abe72d05d7400fe1e8262
jhungerman <joseph_hungerman@yahoo.com>
2022-04-14 03:36:24 -0500

    Completed About models

---
+++ JosephHungerman/Models/ViewModels/AboutViewModel.cs
@@ -0,0 +1,10 @@
+﻿using JosephHungerman.Models.About;
+
+namespace JosephHungerman.Models.ViewModels
+{
+    public class AboutViewModel
+    {
+        public Quote Quote { get; set; }
+        public IList<Section> Sections { get; set; }
+    }
+}
